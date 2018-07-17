f8bb3110ff2f0cbc4fa57096400de9ed587d5eb3
Zeeshan Shahid <zeeshan.shahid@zaths.tech>
2018-07-17 12:39:40 +0500

    init commit

---
+++ app/src/main/java/com/example/dell/newsarticlesummarizer/ui/LoginActivity.java
@@ -0,0 +1,91 @@
+package com.example.dell.newsarticlesummarizer.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.example.dell.newsarticlesummarizer.R;
+import com.example.dell.newsarticlesummarizer.utils.Constants;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private SignInButton signInButton;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        if(account == null){
+            // show signInButton
+            signInButton.setVisibility(View.VISIBLE);
+        } else {
+            // you are already signed
+            signInButton.setVisibility(View.GONE);
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, Constants.RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == Constants.RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            updateUI(account);
+        } catch (ApiException e) {
+            Log.w("Google Error", "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/dell/newsarticlesummarizer/utils/Constants.java
@@ -0,0 +1,5 @@
+package com.example.dell.newsarticlesummarizer.utils;
+
+public class Constants {
+    public static final Integer RC_SIGN_IN = 456;
+}
